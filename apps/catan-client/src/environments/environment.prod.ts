230d315de39c4da17cc91f132726bba99b629027
Christian Ingermann <christian-manuel.ingermann@stud.uni-bamberg.de>
2026-05-25 16:00:30 +0200

    feat: add app version to production environment configuration

--- apps/catan-client/src/environments/environment.prod.ts
+++ apps/catan-client/src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ import { ProductionClientApiOrigin } from '@catan/api-interfaces';
 export const environment = {
   production: true,
   apiBaseUrl: ProductionClientApiOrigin.SameHostRelative,
+  appVersion: '0.0.0',
 };
